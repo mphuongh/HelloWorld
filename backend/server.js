ff54a2e28840c9e1b249b040159e62e3d4c893f1
Minphuong <minphuong0126@gmail.com>
2025-09-30 01:04:56 +0700

    [UPDATE] middleware in server.js

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://hello-world-rosy-beta.vercel.app', 'http://localhost:5173']
+  origin: ['https://hello-world-rosy-beta.vercel.app', 'http://localhost:5173'],
+  methods: ['GET', 'POST'],
+  credentials: true
 }));
+
 app.use(express.json());
 
 // Hello World data with different effects
