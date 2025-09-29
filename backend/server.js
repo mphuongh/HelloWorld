83642225ca8c378a8734c9a310b6970bf427d08e
Minphuong <minphuong0126@gmail.com>
2025-09-30 00:49:22 +0700

    [UPDATE] Front-end link to server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://hello-world-rosy-beta.vercel.app', 'http://localhost:5173']
+}));
 app.use(express.json());
 
 // Hello World data with different effects
